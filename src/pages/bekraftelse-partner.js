6a21bd9e052bb9bd2f57f86b8af9550e5b6be571
oscarpeyron <peyronoscar@gmail.com>
2020-09-29 20:25:22 +0200

    SEO title to confirmation page

--- src/pages/bekraftelse-partner.js
+++ src/pages/bekraftelse-partner.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
-import { Success } from '../components/'
+import { SEO, Success } from '../components/'
 
 const SuccessPartnerPage = () => {
    return (
-      <Success>
-         <h1>Hej!</h1>
-         <p>Tack för er ansökan om att bli Samarbetspartner.<br />Detta är en bekräftelse att Let's Furnish har mottagit er ansökan och kommer att behandla denna inom kort. Återkoppling sker vanligtvis inom 48 timmar.</p>
-      </Success>
+      <>
+         <SEO title="Tack för ditt meddelande!" />
+         <Success>
+            <h1>Hej!</h1>
+            <p>Tack för er ansökan om att bli Samarbetspartner.<br />Detta är en bekräftelse att Let's Furnish har mottagit er ansökan och kommer att behandla denna inom kort. Återkoppling sker vanligtvis inom 48 timmar.</p>
+         </Success>
+      </>
    )
 }
 
